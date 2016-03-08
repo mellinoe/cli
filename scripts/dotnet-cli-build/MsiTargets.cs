9a216f7a8a6d76f0acb93357189e97c004e6e7d0
Matt Ellis <matell@microsoft.com>
2016-03-08 15:22:33 -0800

    Add Shared Host MSI Authoring
    Ideally in the project.json for the shared host we would just list the
    actual package that holds the shared host, instead of all of
    NetStandard.Library, but doing some leads to compliation errors, since
    publish wants to include a compile step that has a generated
    AssemblyAttributes file which references types like System.String.

--- scripts/dotnet-cli-build/MsiTargets.cs
+++ scripts/dotnet-cli-build/MsiTargets.cs
@@ -26,6 +26,8 @@ namespace Microsoft.DotNet.Cli.Build
 
         private static string SdkBundle { get; set; }
 
+        private static string SharedHostMsi { get; set; }
+
         private static string Engine { get; set; }
 
         private static string MsiVersion { get; set; }
@@ -64,6 +66,8 @@ namespace Microsoft.DotNet.Cli.Build
             SdkMsi = Path.ChangeExtension(SdkBundle, "msi");
             Engine = Path.Combine(Path.GetDirectoryName(SdkBundle), ENGINE);
 
+            SharedHostMsi = Path.ChangeExtension(c.BuildContext.Get<string>("SharedHostInstallerFile"), "msi");
+
             var buildVersion = c.BuildContext.Get<BuildVersion>("BuildVersion");
             MsiVersion = buildVersion.GenerateMsiVersion();
             CliVersion = buildVersion.SimpleVersion;
@@ -74,7 +78,7 @@ namespace Microsoft.DotNet.Cli.Build
         }
 
         [Target(nameof(MsiTargets.InitMsi),
-        nameof(GenerateDotnetMuxerMsi),
+        nameof(GenerateDotnetSharedHostMsi),
         nameof(GenerateDotnetSharedFxMsi),
         nameof(GenerateCliSdkMsi))]
         [BuildPlatforms(BuildPlatform.Windows)]
@@ -95,10 +99,24 @@ namespace Microsoft.DotNet.Cli.Build
             return c.Success();
         }
 
-        [Target]
+        [Target(nameof(SharedFrameworkTargets.PublishSharedHost))]
         [BuildPlatforms(BuildPlatform.Windows)]
-        public static BuildTargetResult GenerateDotnetMuxerMsi(BuildTargetContext c)
+        public static BuildTargetResult GenerateDotnetSharedHostMsi(BuildTargetContext c)
         {
+            string WixObjRoot = Path.Combine(Dirs.Output, "obj", "wix", "sharedhost");
+
+            if (Directory.Exists(WixObjRoot))
+            {
+                Directory.Delete(WixObjRoot, true);
+            }
+
+            Directory.CreateDirectory(WixObjRoot);
+
+            Cmd("powershell", "-NoProfile", "-NoLogo",
+                Path.Combine(Dirs.RepoRoot, "packaging", "host", "windows", "generatemsi.ps1"),
+                c.BuildContext.Get<string>("SharedHostPublishRoot"), SharedHostMsi, WixRoot, MsiVersion, CliVersion, Arch, WixObjRoot)
+                    .Execute()
+                    .EnsureSuccessful();
             return c.Success();
         }
 

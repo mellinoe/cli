1de2d11a3bc270eac3d3050bfc8f25b2ca38545b
Eric Mellino <erme@microsoft.com>
2016-10-20 14:38:59 -0700

    Hack

--- init-tools.sh
+++ init-tools.sh
@@ -14,7 +14,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-$DIR/scripts/obtain/dotnet-install.sh --channel $CHANNEL --verbose
+$DIR/scripts/obtain/dotnet-install.sh --channel $CHANNEL --verbose --version 1.0.0-preview3-fakever0
 
 __init_tools_log=$DIR/init-tools.log
 __BUILD_TOOLS_CLI_VERSION=$(cat "$DIR/BuildToolsCliVersion.txt")
